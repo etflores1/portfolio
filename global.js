0a56ad6a8724bcfc3bc1da1324c660ac4d5538b9
etflores1 <etflores@ucsd.edu>
2025-01-25 05:18:08 -0800

    trying to fix links

--- global.js
+++ global.js
@@ -16,37 +16,38 @@ function $$(selector, context = document) {
 // }
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' },
-  { url: 'https://github.com/etflores1', title: 'GitHub Profile' },
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/adrianapsay', title: 'GitHub Profile' },
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
+    let url = p.url;
+    let title = p.title;
 
-  const ARE_WE_HOME = document.documentElement.classList.contains('home');
-  if (!ARE_WE_HOME && !url.startsWith('http')) {
-    url = '../' + url;
-  }
+    url = !ARE_WE_HOME && !url.startsWith('http') ? 'portfolio/' + url : url;
 
-  let a = document.createElement('a');
-  a.href = url;
-  a.textContent = title;
-  nav.append(a);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
 
-  if (a.host === location.host && a.pathname === location.pathname) {
-    a.classList.add('current');
-  }
-
-  if (a.host !== location.host) {
-    a.target = "_blank";
-  }
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+    
+    if (a.host != location.host) {
+        a.target = "_blank"
+    }
+    
 }
 
 document.body.insertAdjacentHTML(
